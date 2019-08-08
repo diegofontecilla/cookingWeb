d9215e429c92bcb0e157cc4612642ef3d5e9a025
diegofontecilla <mail@diegofontecilla.com>
2019-08-08 14:03:14 +0100

    remote build

---
+++ mockApi/public/mockApi.js
@@ -0,0 +1,35 @@
+{
+  "count": 30,
+  "recipes": [
+    {
+    "publisher": "The Pioneer Woman",
+    "f2f_url": "http://food2fork.com/view/47025",
+    "title": "Pasta with Pesto Cream Sauce",
+    "source_url": "http://thepioneerwoman.com/cooking/2011/06/pasta-with-pesto-cream-sauce/",
+    "recipe_id": "47025",
+    "image_url": "http://static.food2fork.com/pestoa0e7.jpg",
+    "social_rank": 100,
+    "publisher_url": "http://thepioneerwoman.com"
+    },
+    {
+    "publisher": "The Pioneer Woman",
+    "f2f_url": "http://food2fork.com/view/8f3e73",
+    "title": "The Best Lasagna Ever",
+    "source_url": "http://thepioneerwoman.com/cooking/2007/06/the_best_lasagn/",
+    "recipe_id": "8f3e73",
+    "image_url": "http://static.food2fork.com/387114468_aafd1be3404a2f.jpg",
+    "social_rank": 100,
+    "publisher_url": "http://thepioneerwoman.com"
+    },
+    {
+    "publisher": "The Pioneer Woman",
+    "f2f_url": "http://food2fork.com/view/47032",
+    "title": "Shrimp Scampi",
+    "source_url": "http://thepioneerwoman.com/cooking/2011/04/16-minute-meal-shrimp-scampi/",
+    "recipe_id": "47032",
+    "image_url": "http://static.food2fork.com/scampibf5a.jpg",
+    "social_rank": 100,
+    "publisher_url": "http://thepioneerwoman.com"
+    }
+  ]
+}
--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -3,21 +3,34 @@ $(document).ready(function(){
   $('#choose-ingredient').submit(function(e){
     e.preventDefault();
     var ingredient = $('#your-ingredient').val();
-    displayRecipe(ingredient);
+    displayRecipes(ingredient);
   })
 
-  function displayRecipe(ingredient) {
-    let url = 'https://www.food2fork.com/api/search'
-    let params = 'key=611435ce5a67fe17c25d250242fe3c6d&q=' + ingredient
+  function displayRecipes(ingredient) {
+
+      // let url = 'http://localhost:9292/'
+      // let params = ''
+
+      let url = 'https://www.food2fork.com/api/search'
+      let params = 'key=611435ce5a67fe17c25d250242fe3c6d&q=' + ingredient
+
+
     $.get(url, params, function(res) {
       let recipesList = jQuery.parseJSON(res)
-      let randomRecipe = Math.floor(Math.random() * 29);
-      let yourRecipe = recipesList.recipes[randomRecipe]
-      console.log(yourRecipe)
-      console.log(yourRecipe.title)
-      console.log(yourRecipe.source_url)
-      $("#link[href='']").attr('href', yourRecipe.source_url);
-      $('#your-recipe').text(yourRecipe.title)
+      let yourRecipeOne = recipesList.recipes[randomRecipe()]
+      let yourRecipeTwo = recipesList.recipes[randomRecipe()]
+      let yourRecipeThree = recipesList.recipes[randomRecipe()]
+
+      $("#link-1[href='']").attr('href', yourRecipeOne.source_url);
+      $('#your-recipe-1').text(yourRecipeOne.title)
+      $("#link-2[href='']").attr('href', yourRecipeTwo.source_url);
+      $('#your-recipe-2').text(yourRecipeTwo.title)
+      $("#link-3[href='']").attr('href', yourRecipeThree.source_url);
+      $('#your-recipe-3').text(yourRecipeThree.title)
     })
+  }
+
+  function randomRecipe() {
+      return Math.floor(Math.random() * 29);
   }
 })
