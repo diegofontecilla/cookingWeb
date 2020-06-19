2125ec497fc1ea772beb5549400749f376b49a40
diegofontecilla <mail@diegofontecilla.com>
2020-06-19 11:27:43 +0100

    delete file

--- delete_this.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-
-def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
-
-print(get_git_revision_short_hash())
